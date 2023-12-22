c8765e8e6dc2167be9de7c0b277cb8f0af014d42
zxw <819400842@qq.com>
2023-12-22 18:02:36 +0800

    no message

--- Unity/Assets/Scripts/StepUdpAsync/UdpAsync.cs
+++ Unity/Assets/Scripts/StepUdpAsync/UdpAsync.cs
@@ -11,7 +11,7 @@ public class UdpAsync : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+    
 
 
     }
---
+++ Unity/CreateProto/readme.txt
@@ -0,0 +1,7 @@
+禁止目录有中文
+
+1,目录 运行。cmd 
+.\protoc -I=配置目录 --csharp_out=输出路径  配置文件.proto
+
+2.在cmd中拖入 protoc.exe 文件
+-I=配置目录 --csharp_out=输出路径  配置文件.proto
